bd06900f0a6429ecc39d3452871a950b04e6e235
Devyn Rucker <devynrucker2017@gmail.com>
2022-03-30 13:53:34 -0400

    first commit

---
+++ src/test/java/com/hello/pms/HelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
